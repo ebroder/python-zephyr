cd10b14e7974b6b9acffaf4159ea60d028c26aee
Evan Broder <broder@mit.edu>
2008-09-26 11:03:13 -0400

    Send zephyrs through ZNotice class
    Includes code to convert ZNotice and ZNotice_t between each other

--- _zephyr.pyx
+++ _zephyr.pyx
@@ -1,27 +1,41 @@
 import os
 import pwd
+import time
 
 cdef extern from "netinet/in.h":
     struct in_addr:
         int s_addr
 
+cdef extern from "arpa/inet.h":
+    char * inet_ntoa(in_addr)
+    int inet_aton(char *, in_addr *)
+
 cdef extern from "zephyr/zephyr.h":
-    struct _ZTimeval_t:
+    ctypedef enum ZNotice_Kind_t:
+        UNSAFE,
+        UNACKED,
+        ACKED,
+        HMACK,
+        HMCTL,
+        SERVACK,
+        SERVNAK,
+        CLIENTACK,
+        STAT
+    
+    struct _ZTimeval:
         int tv_sec
         int tv_usec
-    ctypedef _ZTimeval_t ZTimeval_t
     
-    struct _ZUnique_Id_t:
+    ctypedef struct ZUnique_Id_t:
         in_addr zuid_addr
-        ZTimeval_t tv
-    ctypedef _ZUnique_Id_t ZUnique_Id_t
+        _ZTimeval tv
     
-    struct _ZNotice_t:
+    ctypedef struct ZNotice_t:
         char * z_packet
         char * z_version
-        int z_kind
+        ZNotice_Kind_t z_kind
         ZUnique_Id_t z_uid
-        ZTimeval_t z_time
+        _ZTimeval z_time
         unsigned short z_port
         int z_auth
         int z_checked_auth
@@ -40,7 +54,6 @@ cdef extern from "zephyr/zephyr.h":
         char * z_other_fields[10]
         char * z_message
         int z_message_len
-    ctypedef _ZNotice_t ZNotice_t
     
     int (*ZAUTH)()
     int (*ZNOAUTH)()
@@ -57,6 +70,8 @@ cdef extern from "stdlib.h":
 
 cdef extern from "string.h":
     void * memset(void *, int, unsigned int)
+
+### BEGIN autoinitialization
 
 cdef unsigned short __port
 __port = 0
@@ -81,32 +96,131 @@ cdef __openPort(unsigned short * port):
     errno = ZOpenPort(port)
     __error(errno)
 
-def port():
-    return __port
+### END autoinitialization
 
-def zwrite(recipient='', message='', cls='message', instance='personal', opcode='', sender=None, auth=True, zsig=None, **options):
-    cdef ZNotice_t notice
-    memset(&notice, 0, sizeof(ZNotice_t))
-    
-    if zsig is not None:
-        sig = options['zsig']
+cdef object _string_c2p(char * string):
+    if string is NULL:
+        return None
     else:
-        sig = pwd.getpwuid(os.getuid()).pw_gecos.split(',')[0]
-    full_message = '%s\0%s' % (sig, message)
-    
-    if sender is not None:
-        notice.z_sender = sender
-    
-    notice.z_kind = 2
-    notice.z_class = cls
-    notice.z_class_inst = instance
-    notice.z_recipient = recipient
-    notice.z_message = full_message
-    notice.z_message_len = len(full_message)
-    notice.z_opcode = opcode
-    
-    if auth:
-        errno = ZSendNotice(&notice, ZAUTH)
+        return str(string)
+
+cdef char * _string_p2c(object string):
+    if string is None:
+        return NULL
     else:
-        errno = ZSendNotice(&notice, ZNOAUTH)
-    __error(errno)
+        return string
+
+# Timevals can just be floats; they're not intereting enough to have
+# their own class
+
+cdef object _ZTimeval_c2p(_ZTimeval * timeval):
+    return timeval.tv_sec + (timeval.tv_usec / 100000.0)
+
+cdef void _ZTimeval_p2c(float timeval, _ZTimeval * c_timeval) except *:
+    c_timeval.tv_sec = int(timeval)
+    c_timeval.tv_usec = int((timeval - c_timeval.tv_sec) * 100000)
+
+class ZUid():
+    """
+    A per-transaction unique ID for zephyrs
+    """
+    __slots__ = ('address', 'time')
+    
+    def __init__(self):
+        self.address = ''
+        self.time = 0
+
+cdef object _ZUid_c2p(ZUnique_Id_t * uid):
+    p_uid = ZUid()
+    p_uid.address = inet_ntoa(uid.zuid_addr)
+    p_uid.time = _ZTimeval_c2p(&uid.tv)
+    
+    return p_uid
+
+cdef void _ZUid_p2c(object uid, ZUnique_Id_t * c_uid) except *:
+    inet_aton(uid.address, &c_uid.zuid_addr)
+    _ZTimeval_p2c(uid.time, &c_uid.tv)
+
+class ZNotice():
+    """
+    A zephyr message
+    """
+    __slots__ = ('kind', 'uid', 'time', 'port', 'auth',
+                 'cls', 'instance', 'recipient',
+                 'sender', 'opcode', 'format',
+                 'fields',
+                 'message')
+    
+    def __init__(self):
+        self.kind = ACKED
+        self.cls = 'message'
+        self.instance = 'personal'
+        
+        self.uid = ZUid()
+        self.time = 0
+        self.port = 0
+        self.auth = True
+        self.recipient = None
+        self.sender = None
+        self.opcode = None
+        self.format = None
+        self.fields = []
+        self.message = None
+    
+    def send(self):
+        cdef ZNotice_t notice
+        _ZNotice_p2c(self, &notice)
+        
+        if self.auth:
+            errno = ZSendNotice(&notice, ZAUTH)
+        else:
+            errno = ZSendNotice(&notice, ZNOAUTH)
+        __error(errno)
+
+cdef object _ZNotice_c2p(ZNotice_t * notice):
+    p_notice = ZNotice()
+    
+    p_notice.kind = notice.z_kind
+    p_notice.uid = _ZUid_c2p(&notice.z_uid)
+    p_notice.time = _ZTimeval_c2p(&notice.z_time)
+    p_notice.port = int(notice.z_port)
+    p_notice.auth = bool(notice.z_auth)
+    
+    p_notice.cls = _string_c2p(notice.z_class)
+    p_notice.instance = _string_c2p(notice.z_class_inst)
+    p_notice.recipient = _string_c2p(notice.z_recipient)
+    p_notice.sender = _string_c2p(notice.z_sender)
+    p_notice.opcode = _string_c2p(notice.z_opcode)
+    p_notice.format = _string_c2p(notice.z_default_format)
+    p_notice.fields = list()
+    for i in range(notice.z_num_other_fields):
+        p_notice.fields.append(notice.z_other_fields[i])
+    
+    p_notice.message = _string_c2p(notice.z_message)
+    
+    return p_notice
+
+cdef void _ZNotice_p2c(object notice, ZNotice_t * c_notice) except *:
+    memset(c_notice, 0, sizeof(ZNotice_t))
+    
+    c_notice.z_kind = notice.kind
+    _ZUid_p2c(notice.uid, &c_notice.z_uid)
+    if notice.time != 0:
+        _ZTimeval_p2c(notice.time, &c_notice.z_time)
+    if notice.port != 0:
+        c_notice.z_port = notice.port
+    c_notice.z_auth = int(notice.auth)
+    
+    c_notice.z_class = _string_p2c(notice.cls)
+    c_notice.z_class_inst = _string_p2c(notice.instance)
+    c_notice.z_recipient = _string_p2c(notice.recipient)
+    c_notice.z_sender = _string_p2c(notice.sender)
+    c_notice.z_opcode = _string_p2c(notice.opcode)
+    c_notice.z_default_format = _string_p2c(notice.format)
+    c_notice.z_num_other_fields = len(notice.fields)
+    for i in range(c_notice.z_num_other_fields):
+        c_notice.z_other_fields[i] = _string_p2c(notice.fields[i])
+    
+    c_notice.z_message = _string_p2c(notice.message)
+    c_notice.z_message_len = len(notice.message)
+
